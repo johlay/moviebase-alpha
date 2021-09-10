510e8c9803e70d39b303a1d51f8996f284687b9e
Johnny Lay <johnny.lay.developer@gmail.com>
2021-09-10 16:37:27 +0200

    Changed default bg color of App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Navigation from "./components/navigation/Navigation";
+import HomePage from "./pages/HomePage";
+import GenresPage from "./pages/GenresPage";
 
 function App() {
   // create a react query - client
   const queryClient = new QueryClient();
+
+  // change background color of whole app.
+  document.body.className = "bg-secondary";
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -14,7 +19,15 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Navigation />
-          <Switch>{/* <Route exact path="/"></Route> */}</Switch>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+
+            <Route path="/genres">
+              <GenresPage />
+            </Route>
+          </Switch>
         </BrowserRouter>
       </div>
     </QueryClientProvider>
---
+++ src/pages/GenresPage.jsx
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/Container";
+
+const GenresPage = () => {
+  return (
+    <Container className="my-3">
+      <h2 className="text-center text-light">
+        <u>Genres</u>
+      </h2>
+    </Container>
+  );
+};
+
+export default GenresPage;
