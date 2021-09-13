56014e3f14a0ad08163538e9c35bfe5463242144
Johnny Lay <johnny.lay.developer@gmail.com>
2021-09-13 13:31:57 +0200

    Only show 5 similiar movies in the carousel

--- src/components/movie/SimiliarMovie.jsx
+++ src/components/movie/SimiliarMovie.jsx
@@ -27,23 +27,27 @@ const SimiliarMovie = ({ movies }) => {
       >
         {movies &&
           movies?.results?.map((movie, index) => {
-            return (
-              <Carousel.Item>
-                <img
-                  className="d-block w-25 mx-auto"
-                  src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <Link
-                    className="btn btn-dark btn-lg"
-                    to={`/movies/${movie.id}`}
-                  >
-                    Go to movie
-                  </Link>
-                </Carousel.Caption>
-              </Carousel.Item>
-            );
+            if (index < 5) {
+              return (
+                <Carousel.Item>
+                  <img
+                    className="d-block w-25 mx-auto"
+                    src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <Link
+                      className="btn btn-dark btn-lg"
+                      to={`/movies/${movie.id}`}
+                    >
+                      Go to movie
+                    </Link>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            } else {
+              return null;
+            }
           })}
       </Carousel>
     </>
