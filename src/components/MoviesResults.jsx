85eaa052705f35b3a02827a9c69cd1776649583c
Johnny Lay <johnny.lay.developer@gmail.com>
2021-09-16 15:42:44 +0200

    Included optional chaining for components and also reducing shown movies for homepage from 20 movies to 10 movies per page.

--- src/components/MoviesResults.jsx
+++ src/components/MoviesResults.jsx
@@ -8,27 +8,27 @@ const MoviesResults = ({ movies }) => {
   return (
     <>
       {movies &&
-        movies.results.map((movie) => {
+        movies?.results?.map((movie) => {
           return (
             <Row className="rounded bg-white g-0 my-4" key={movie.id}>
               <Col xs="auto">
                 <img
                   className={`${styles.movie_poster}`}
-                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
                   alt="movie poster"
                 />
               </Col>
               <Col className="d-flex flex-column justify-content-center ms-2">
-                <h3 className="h5 m-0">{movie.title}</h3>
+                <h3 className="h5 m-0">{movie?.title}</h3>
                 <p>
-                  {movie.overview.split(" ", 7).map((word) => word + " ")}...
+                  {movie?.overview?.split(" ", 7).map((word) => word + " ")}...
                 </p>
                 <p className="m-0">
                   <span className="fw-bold" aria-label="text">
                     Original language:{" "}
                   </span>
                   <span aria-label="the movie's language">
-                    {movie.original_language.toUpperCase()}
+                    {movie?.original_language.toUpperCase()}
                   </span>
                 </p>
                 <p>
@@ -36,7 +36,7 @@ const MoviesResults = ({ movies }) => {
                     Release date:{" "}
                   </span>
                   <span aria-label="date for release date">
-                    {dayjs(movie.release_date).format("D MMM YYYY")}
+                    {dayjs(movie?.release_date).format("D MMM YYYY")}
                   </span>
                 </p>
               </Col>
@@ -47,7 +47,7 @@ const MoviesResults = ({ movies }) => {
               >
                 <NavLink
                   className="btn btn-dark mb-2 me-3"
-                  to={`/movies/${movie.id}`}
+                  to={`/movies/${movie?.id}`}
                 >
                   Learn more
                 </NavLink>
--- src/components/home/MoviesList.jsx
+++ src/components/home/MoviesList.jsx
@@ -10,33 +10,34 @@ const MoviesList = ({ movies }) => {
   return (
     <div className={`${styles.movies_container}`}>
       {movies &&
-        movies.results.map((movie) => {
-          return (
-            <Card
-              className={`${styles.movie_card}`}
-              key={movie.id}
-              onClick={() => history.push(`/movies/${movie.id}`)}
-            >
-              <Card.Img
-                className={`${styles.movie_poster}`}
-                variant="top"
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt="movie poster"
-              />
-              <Card.Body>
-                <Card.Title>{movie?.title}</Card.Title>
-                <Card.Text>
-                  <span
-                    aria-label="icon of star with movie rating"
-                    className="me-2"
-                  >
-                    <FontAwesomeIcon icon={faStar} size="1x" color="yellow" />
-                  </span>
-                  {movie.vote_average}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          );
+        movies.results.map((movie, index) => {
+          if (index < 10)
+            return (
+              <Card
+                className={`${styles.movie_card}`}
+                key={movie?.id}
+                onClick={() => history.push(`/movies/${movie.id}`)}
+              >
+                <Card.Img
+                  className={`${styles.movie_poster}`}
+                  variant="top"
+                  src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+                  alt="movie poster"
+                />
+                <Card.Body>
+                  <Card.Title>{movie?.title}</Card.Title>
+                  <Card.Text>
+                    <span
+                      aria-label="icon of star with movie rating"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon icon={faStar} size="1x" color="yellow" />
+                    </span>
+                    {movie?.vote_average}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
         })}
     </div>
   );
