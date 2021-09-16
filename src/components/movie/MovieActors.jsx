014482dd88eaa94e791261fde42aeffea0b98713
Johnny Lay <johnny.lay.developer@gmail.com>
2021-09-16 20:18:29 +0200

    Re-located code to own component.

--- src/components/movie/MovieActors.jsx
+++ src/components/movie/MovieActors.jsx
@@ -1,11 +1,7 @@
-import styles from "../../css/MovieActors.module.css";
 import { useHistory } from "react-router";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { getImage } from "../../helpers";
-// import MovieActorDetail from "./MovieActorDetail";
+import MovieActorDetail from "./MovieActorDetail";
 
 const MovieActors = ({ actors }) => {
   const history = useHistory();
@@ -19,28 +15,7 @@ const MovieActors = ({ actors }) => {
           actors.map((actor) => {
             if (actor.profile_path === null) return null;
 
-            {
-              /* <><MovieActorDetail actor={actor} id={actor.id}/></> */
-            }
-            return (
-              <Col xs="auto" key={actor?.id}>
-                <Card
-                  onClick={() => history.push(`/actors/${actor?.id}`)}
-                  className={`${styles.actor_card} text-dark text-center mx-3 mb-3`}
-                >
-                  <Card.Img
-                    variant="top"
-                    src={getImage(200, actor?.profile_path)}
-                  />
-                  <Card.Body>
-                    <Card.Title>{actor?.name}</Card.Title>
-                    <Card.Text className="fst-italic text-center">
-                      {actor?.character}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
+            return <MovieActorDetail actor={actor} id={actor.id} />;
           })}
       </Row>
     </Container>
