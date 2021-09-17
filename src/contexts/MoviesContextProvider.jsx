cfc034a2ec4327902741ecb7c413f4c254781649
Johnny Lay <johnny.lay.developer@gmail.com>
2021-09-17 09:34:17 +0200

    Added code logic for handling incoming movie data before saving to "recently-viewed" in local storage.

--- src/contexts/MoviesContextProvider.jsx
+++ src/contexts/MoviesContextProvider.jsx
@@ -5,11 +5,13 @@ export const MoviesContext = createContext();
 
 const MoviesContextProvider = ({ children }) => {
   const [value, setValue] = useLocalStorage("recently-viewed");
-  const [recentMovies, setRecentMovies] = useState("");
+  const [recentMovies, setRecentMovies] = useState({ results: [] });
 
   useEffect(() => {
     if (!value) {
       console.log("storage key is NOT found");
+
+      return;
     }
 
     // when component mounts, check if storage key: "recently-viewed" is stored in local storage. If true, load it to state.
@@ -20,7 +22,61 @@ const MoviesContextProvider = ({ children }) => {
     }
   }, []);
 
-  return <MoviesContext.Provider value={{}}>{children}</MoviesContext.Provider>;
+  useEffect(() => {
+    // as soon as state: "recentMovies" updates then save information to local-storage
+    if (recentMovies.results.length > 0) setValue(recentMovies);
+  }, [recentMovies]);
+
+  // check the incoming data
+  const checkMovieData = (movieData) => {
+    // variable for checking if state: "recentMovies" contains the same movie.
+    const movieExists = recentMovies?.results?.some(
+      (movie) => movie.id === movieData.id
+    );
+
+    // check if state: "recentMovies" contains the same movie. If true, replace that existing movie with index position: 0
+    if (movieExists) {
+      // create a new movie list EXCEPT the already existing movie.
+      const newList = recentMovies.results.filter(
+        (movie) => movie.id !== movieData.id
+      );
+
+      // add the "existing movie" to top position of array (index position: 0)
+      newList.unshift(movieData);
+
+      // save new list to state: "recentMovies"
+      setRecentMovies({ results: [...newList] });
+
+      return;
+    }
+
+    // check if state: "recentMovies" contains more than 10 movies.
+    if (recentMovies.results.length >= 10) {
+      // only return/save the recently viewed movies (9 movies from this list which consists of 10 movies.)
+      const newList = recentMovies.results.filter((movie, index) => {
+        if (index < 9) return movie;
+        else return "";
+      });
+
+      // save new list to state: "recentMovies"
+      setRecentMovies({ results: [movieData, ...newList] });
+
+      return;
+    }
+
+    // if state: "recentMovies" contains less than 11 movies, then spread out that list of recentMovies and add the "recently viewed movie" to first position of the new array.
+    if (recentMovies.results.length < 10) {
+      setRecentMovies({ results: [movieData, ...recentMovies.results] });
+
+      return;
+    }
+  };
+
+  return (
+    <MoviesContext.Provider value={{ checkMovieData }}>
+      {children}
+    </MoviesContext.Provider>
+  );
 };
 
 export default MoviesContextProvider;
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -5,7 +5,7 @@ import { getSpecificMovie } from "../services/TmdbApi";
 import MovieDetails from "../components/movie/MovieDetails";
 import MovieActors from "../components/movie/MovieActors";
 import SimiliarMovies from "../components/movie/SimiliarMovies";
-import useLocalStorage from "../hooks/useLocalStorage";
+import useMovies from "../hooks/useMovies";
 
 const MoviePage = () => {
   const { movieId } = useParams();
@@ -13,48 +13,49 @@ const MoviePage = () => {
     getSpecificMovie(movieId)
   );
 
-  const [value, setValue] = useLocalStorage("recently-viewed");
+  
+
+  const { checkMovieData } = useMovies();
 
   useEffect(() => {
-    // First check if there is data about movie - if true: as soon as user enters "page for specific movie" then save details about the movie to local storage.
     if (data) {
-      // variable for checking if local storage key: "recently-viewed" contains the same movie.
-      const movieExists = value?.results?.some((movie) => movie.id === data.id);
-
-      // check if local storage key: "recently-viewed" contains the same movie. If true, replace that existing movie with index position: 0.
-      if (movieExists) {
-        // create a new movie list EXCEPT the already existing movie.
-        const newList = value?.results?.filter((movie) => movie.id !== data.id);
-
-        // adds the "existing movie" to top position of array (index position: 0)
-        newList.unshift(data);
-
-        // save new list to local storage.
-        setValue({ results: [...newList] });
-
-        return;
-      }
-
-      // check if local storage key: "recently-viewed" has 10 movies.
-      if (value?.results?.length >= 10) {
-        // only returns/saves the recently viewed movies (9 movies() from this list which consists of 10 movies.
-        const newList = value?.results?.filter((movie, index) => {
-          if (index < 9) return movie;
-          else return "";
-        });
-
-        // save new list to local storage.
-        setValue({ results: [data, ...newList] });
-
-        return;
-      }
-
-      // If data is found in local storage, then spread out that list of data and add the "recent viewed movie" to first position of the new array.
-      value
-        ? setValue({ results: [data, ...value.results] })
-        : setValue({ results: [data] });
+      // function for handling incoming movie data - before saving data to "recently-viewed" movies in local storage.
+      checkMovieData(data);
     }
   }, [data]);
+
+  // useEffect(() => {
+  //   // First check if there is data about movie - if true: as soon as user enters "page for specific movie" then save details about the movie to local storage.
+  //   if (data) {
+  //     // variable for checking if local storage key: "recently-viewed" contains the same movie.
+  //     const movieExists = value?.results?.some((movie) => movie.id === data.id);
+  //     // check if local storage key: "recently-viewed" contains the same movie. If true, replace that existing movie with index position: 0.
+  //     if (movieExists) {
+  //       // create a new movie list EXCEPT the already existing movie.
+  //       const newList = value?.results?.filter((movie) => movie.id !== data.id);
+  //       // adds the "existing movie" to top position of array (index position: 0)
+  //       newList.unshift(data);
+  //       // save new list to local storage.
+  //       setValue({ results: [...newList] });
+  //       return;
+  //     }
+  //     // check if local storage key: "recently-viewed" has 10 movies.
+  //     if (value?.results?.length >= 10) {
+  //       // only returns/saves the recently viewed movies (9 movies() from this list which consists of 10 movies.
+  //       const newList = value?.results?.filter((movie, index) => {
+  //         if (index < 9) return movie;
+  //         else return "";
+  //       });
+  //       // save new list to local storage.
+  //       setValue({ results: [data, ...newList] });
+  //       return;
+  //     }
+  //     // If data is found in local storage, then spread out that list of data and add the "recent viewed movie" to first position of the new array.
+  //     value
+  //       ? setValue({ results: [data, ...value.results] })
+  //       : setValue({ results: [data] });
+  //   }
+  // }, [data]);
 
   return (
     <>
